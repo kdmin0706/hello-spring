cdddde71aee2192896ce0a9a1a84aa1c8f2041cd
kdmin0706 <gxxk76@gmail.com>
2023-11-13 01:30:50 +0900

    commit

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,20 +1,50 @@
 package hello.hellospring.config;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
+    /* [JPA]
+        private final EntityManager entityManager;
+
+        @Autowired
+        public SpringConfig(EntityManager entityManager) {
+            this.entityManager = entityManager;
+        }
+     */
+
+    /* [JDBC]
+        private final DataSource dataSource;
+
+        @Autowired
+        public SpringConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+     */
+
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(entityManager);
+//    }
 }
