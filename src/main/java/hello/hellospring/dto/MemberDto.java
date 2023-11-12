62db46fb1707669e8d62c8f12d0aa95784c82ab7
kdmin0706 <gxxk76@gmail.com>
2023-11-13 01:28:30 +0900

    member 관련 내용 커밋

---
+++ src/main/java/hello/hellospring/dto/MemberDto.java
@@ -0,0 +1,13 @@
+package hello.hellospring.dto;
+
+public class MemberDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
